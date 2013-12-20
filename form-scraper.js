36e54f68916c34e2f90fc4e180288ba51a411327
tounano <bitbucket@clickhere.co.il>
2013-12-20 14:02:52 +0200

    Refactored form scraper

--- form-scraper.js
+++ form-scraper.js
@@ -6,23 +6,42 @@ var ScrapingFormProvider = function() {};
 
 _.extend(ScrapingFormProvider, {
   provideForm: function (formId, url, promisifiedRequest) {
+    var $;
     return promisifiedRequest.get(url)
-      .then( function (response) {
-        var $ = cheerio.load(response.body);
-        var formDom = $(formId);
+      .then(fetchFormDataFromHttpResponse);
 
-        if (formDom.length < 1)
-          return when.reject("ERROR_FORM_IS_ABSENT");
+    function fetchFormDataFromHttpResponse(response) {
+      $ = cheerio.load(response.body);
+      var formDom = $(formId);
 
-        var formData = {};
-        formData.action = formDom.attr('action');
-        formData.data = {};
-        formDom.find("input").each( function () {
-          formData.data[$(this).attr("name")] = $(this).val();
-        });
+      if (isFormAbsent(formDom))
+        return when.reject(C.ERROR_FORM_IS_ABSENT);
 
-        return when.resolve(formData);
+      return when.resolve(fetchFormDataFrom(formDom));
+    }
+
+    function fetchFormDataFrom(formDom) {
+      var formData = {};
+      provideActionData(formDom, formData);
+      provideInputData(formDom, formData);
+
+      return formData;
+    }
+
+    function isFormAbsent(formDom) {
+      return formDom.length < 1;
+    }
+
+    function provideActionData(formDom, formData) {
+      formData.action = formDom.attr('action');
+    }
+
+    function provideInputData(formDom, formData) {
+      formData.data = {};
+      formDom.find("input").each( function () {
+        formData.data[$(this).attr("name")] = $(this).val();
       });
+    }
   }
 })
 
@@ -37,6 +56,10 @@ var testOptions = {
   promisifiedRequest: "",
   formUrl: "",
   formId: ""
+}
+
+var C = {
+  ERROR_FORM_IS_ABSENT: "ERROR_FORM_IS_ABSENT"
 }
 
 exports.ScrapingFormProvider = ScrapingFormProvider;
